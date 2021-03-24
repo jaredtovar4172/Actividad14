acd4ce32019c031b595ee3f7aed2b2242d41c9b2
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-23 21:10:48 -0600

    Add files via upload

---
+++ Ej14_CuadradoDeEnteros/Ej14_CuadradoDeEnteros/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej14_CuadradoDeEnteros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Operacion x = new Operacion();
+
+            decimal b = Convert.ToDecimal(this.textBox1.Text);
+            decimal resultado = x.Op(b);
+            this.textBox2.Text = resultado.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Ej14_CuadradoDeEnteros/Ej14_CuadradoDeEnteros/Operacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej14_CuadradoDeEnteros
+{
+    class Operacion
+    {
+        public decimal Op (decimal b)
+        {
+            decimal aux = b;
+            for (int i = 1; i <= 2 - 1; i++)
+                aux = aux * b;
+            return (aux);
+        }
+}
+}
